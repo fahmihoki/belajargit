f2d5bcd2940ff739d32297ca3ee76f9c5135b036
Fahmi Naufal.R <9h.fahmi.naufal.ridwan@gmail.com>
2017-01-24 12:17:07 +0700

    membuat file index

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+	<h1>ini halaman git</h1>
+	<p>git gud</p>
+</body>
+</html>
+
---
+++ profile.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+	<h1>ini halaman git</h1>
+	<p>git gud</p>
+	<p>lorem ipsum</p>
+</body>
+</html>
+
